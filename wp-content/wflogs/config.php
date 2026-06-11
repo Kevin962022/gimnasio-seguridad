460d1436eb036d209b481514dbc9905ff7b2b2c4
Anonymous <villafuerte.montero.26@gmail.com>
2026-06-11 16:03:48 -0500

    Initial commit - WordPress LeonGym con seguridad

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1781765601;s:7:"authKey";s:64:"JKyltrR#]^0)_5Ecb/~@pEV/O%w/Bpp3r]Dw_l!N@$QHe#i)Bb<&yUbC@3Jg}%M/";s:7:"version";s:5:"1.1.0";}
